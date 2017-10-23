598897725cc61d67208f611ed5729809e9aa1086
Diana M <diana.samboni@codescrum.com>
2017-10-23 12:43:50 -0500

    Basic example using resourses strings defined for product flavors

--- app/src/main/java/com/example/codescrum/moviesexample/MainActivity.java
+++ app/src/main/java/com/example/codescrum/moviesexample/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.codescrum.moviesexample;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt_env;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_env = (TextView) findViewById(R.id.txt_env);
+
+        Resources res = getResources();
+
+        txt_env.setText(res.getString(R.string.env_name));
     }
 }
